d6df9e580a785d9e9ab39de35289f71a3f3f9949
Six <six.bradshaw@gmail.com>
2024-02-06 17:53:51 -0600

    Restyled code

---
+++ app/generate/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SubjectSelectionForm from '../components/SubjectSelectionForm';
+
+
+export default function Generate() {
+  return (
+    <div>
+      <h1 className='text-xl'>Generate Page</h1>
+      <div className='p-4'>
+        <SubjectSelectionForm />
+      </div>
+    </div>
+  );
+}
